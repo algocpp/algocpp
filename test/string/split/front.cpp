f9aed346f18fcd671fb5a0fa9c2a922e2752640a
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-06-02 17:56:35 +0900

    string/split test

---
+++ test/string/split/front.cpp
@@ -0,0 +1,15 @@
+
+#include <bits/stdc++.h>
+#include <string/split.hpp>
+using namespace std;
+
+int main()
+{
+	auto x = algocpp::string::split("ABC", "A");
+	for (int i = 0; i < x.size(); i++)
+	{
+		cout << x[i] << " ";
+	}
+
+	cout << endl;
+}
---
+++ test/string/split/simple.cpp
@@ -0,0 +1,15 @@
+
+#include <bits/stdc++.h>
+#include <string/split.hpp>
+using namespace std;
+
+int main()
+{
+	auto x = algocpp::string::split("ABC", "B");
+	for (int i = 0; i < x.size(); i++)
+	{
+		cout << x[i] << " ";
+	}
+
+	cout << endl;
+}
