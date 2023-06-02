c430cebfe5409dd5373e6e19038e113acb323ecd
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-06-02 20:35:17 +0900

    debug + `fact`

---
+++ test/math/fact/inp.cpp
@@ -0,0 +1,14 @@
+
+#include <bits/stdc++.h>
+#include <algocpp/math/fact.hpp>
+using namespace std;
+
+int main()
+{
+    while (true)
+    {
+        unsigned long long N;
+        cin >> N;
+        cout << algocpp::math::fact(N) << endl;
+    }
+}
