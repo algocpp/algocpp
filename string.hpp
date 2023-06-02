cf1fd3b28d4bdb4f7b537bba40f0f367c3eb85bc
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-06-02 18:22:29 +0900

    add `string.hpp`

---
+++ string.hpp
@@ -0,0 +1,7 @@
+
+// algocpp/includes/string.hpp
+//
+// This file is part of algocpp and is copyrighted by algocpp.
+// If used, it must comply with the MIT License.
+
+#include <algocpp/string/split.hpp>
