17f0fa3b0bfd09174c96ef705945e00766d82de4
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-07-01 14:11:05 +0900

    hai

--- build.bat
+++ build.bat
@@ -4,7 +4,6 @@
 echo Algo.cpp is precompiled.
 echo Do not operate until the end of the program.
 
-cd ../
-g++ algocpp/all.hpp -I . -O3
+g++ includes/algocpp/all.hpp -I includes/ -O3
 
 echo Precompilation of Algo.cpp is complete.
