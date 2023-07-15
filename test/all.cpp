0b73ccfa8deed68a965b3a5da272529ebe8fb73a
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-07-15 11:00:46 +0900

    moved wiki

--- test/all.cpp
+++ test/all.cpp
@@ -1,9 +1,19 @@
 
 #include <iostream>
-#include <algocpp/all.hpp>
+#include <vector>
+#include <algocpp/array.hpp>
 using namespace std;
 
 int main()
 {
-	algocpp::string::replace("AAA", "", "A");
+	vector<int> a = {1, 2, 3, 3};
+
+	// 1
+	cout << algocpp::array::count(a, 1) << endl;
+
+	// 2
+	cout << algocpp::array::count(a, 3) << endl;
+
+	// 0
+	cout << algocpp::array::count(a, 5) << endl;
 }
