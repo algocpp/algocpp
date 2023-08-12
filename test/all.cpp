1027c86b068c1b6a490a47ebbeb4cfadb88be4e2
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-08-12 18:58:25 +0900

    update

--- test/all.cpp
+++ test/all.cpp
@@ -1,9 +1,10 @@
 
-// #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <algocpp/all.hpp>
 // #include <boost/array.hpp>
 using namespace std;
 
 int main()
 {
+	cout << algocpp::string::replace("ABCBA", "B", "?") << endl;
 }
