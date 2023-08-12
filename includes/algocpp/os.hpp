023bf51bece5cc9e747b13a3e81edc41e8d65857
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-08-12 16:10:58 +0900

    fwre

--- includes/algocpp/os.hpp
+++ includes/algocpp/os.hpp
@@ -4,5 +4,5 @@
 // This file is part of algocpp and is copyrighted by algocpp.
 // If used, it must comply with the MIT License.
 
-#include <algocpp/os/get_temp.hpp>
+//#include <algocpp/os/get_temp.hpp>
 #include <algocpp/os/define.hpp>
--- test/all.cpp
+++ test/all.cpp
@@ -1,12 +1,9 @@
 
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 #include <algocpp/all.hpp>
-#include <boost/array.hpp>
+// #include <boost/array.hpp>
 using namespace std;
 
 int main()
 {
-	boost::array<int, 4> a = {3, 5, 7, 2};
-
-	cout << a << endl;
 }
